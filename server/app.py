d8bc4e4ef4515e4151fc6fd3edc6fd735ae473b5
BookmDan <danoh329@gmail.com>
2023-12-10 18:45:34 -0800

    not sure

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from models import db, Bakery, BakedGood
 
@@ -14,14 +15,76 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+class Bakery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+class BakedGood(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakery.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
 @app.route('/')
 def home():
     return '<h1>Bakery GET-POST-PATCH-DELETE API</h1>'
+
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    data = request.form
+    bakery_id = data.get('bakery_id')
+    name = data.get('name')
+    price = data.get('price')
+
+    if bakery_id is None or name is None or price is None:
+        return jsonify({'error': 'Missing required parameters'}), 400
+
+    new_baked_good = BakedGood(bakery_id=bakery_id, name=name, price=price)
+    db.session.add(new_baked_good)
+    db.session.commit()
+
+    return jsonify(new_baked_good.to_dict()), 201
 
 @app.route('/bakeries')
 def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
+
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery(id):
+    bakery = Bakery.query.get(id)
+
+    if bakery is None:
+        return jsonify({'error': 'Bakery not found'}), 404
+
+    data = request.form
+    new_name = data.get('name')
+
+    if new_name:
+        bakery.name = new_name
+
+    db.session.commit()
+
+    return jsonify(bakery.to_dict())
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.get(id)
+
+    if baked_good is None:
+        return jsonify({'error': 'Baked Good not found'}), 404
+
+    db.session.delete(baked_good)
+    db.session.commit()
+
+    return jsonify({'message': 'Baked Good deleted successfully'})
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
